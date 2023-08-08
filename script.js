b24ae3ffc193327be36b228ed3b8ed1f8dfe0115
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2023-08-08 19:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let simpleArray=['one',2,'three',true,false,undefined,null];
+console.log(simpleArray.length);
+let complexArray = [
+  [
+    {
+      one: "string"
+     
+    }
+   
+  ],
+  [
+    {
+      a: 100
+      
+    }
+  ],
+	[
+    
+    {
+      c:true 
+     
+    }
+  ]
+
+	
+];
 
